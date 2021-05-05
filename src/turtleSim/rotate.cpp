81c99879a5c29439ef6ea4b05a4f26cdc535f4eb
mayank10 <31381335+mjoshi07@users.noreply.github.com>
2021-05-05 21:21:11 +0530

    Update rotate.cpp

--- src/turtleSim/rotate.cpp
+++ src/turtleSim/rotate.cpp
@@ -2,17 +2,16 @@
 
 int main(int argc, char** argv)
 {
-
-    ros::init(argc, argv, "turtleBot_rotate");
+	ros::init(argc, argv, "turtleBot_rotate");
 	ros::NodeHandle n;
 	double angularSpeedInDegrees = 10;
-    double relativeAngleInDegress = 90;
+	double relativeAngleInDegress = 90;
 	bool isClockwise = true;
 
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
 
-    rotate(angularSpeedInDegrees, relativeAngleInDegress, isClockwise);
+	rotate(angularSpeedInDegrees, relativeAngleInDegress, isClockwise);
 
-    return 0;
+	return 0;
 }
