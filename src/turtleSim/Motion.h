aa9a6251eb5bcdfe105f6765d90e927fa10cab19
mayankJoshi <31381335+mjoshi07@users.noreply.github.com>
2021-05-11 15:09:50 +0530

    ros::Rate replaced with ros::WallRate and some minor changes

--- src/turtleSim/Motion.h
+++ src/turtleSim/Motion.h
@@ -14,11 +14,11 @@ const double y_max = 11.0;
 
 const double PI = 3.14159265359;
 
-void poseCallback(const turtlesim::Pose::ConstPtr & pose_message)
+void poseCallback(turtlesim::Pose pose_message)
 {
-	turtlesim_pose.x = pose_message->x;
-	turtlesim_pose.y = pose_message->y;
-	turtlesim_pose.theta = pose_message->theta;
+	turtlesim_pose.x = pose_message.x;
+	turtlesim_pose.y = pose_message.y;
+	turtlesim_pose.theta = pose_message.theta;
 }
 
 double degrees2radians(double angle_in_degrees)
@@ -39,12 +39,12 @@ void moveStraight(double linearSpeed, double distance, bool isForward)
 {
     geometry_msgs::Twist vel_msg;
 
-	vel_msg.linear.y =0;
-	vel_msg.linear.z =0;
+	vel_msg.linear.y = 0;
+	vel_msg.linear.z = 0;
 	//set a random angular velocity in the y-axis
 	vel_msg.angular.x = 0;
 	vel_msg.angular.y = 0;
-	vel_msg.angular.z =0;
+	vel_msg.angular.z = 0;
 
     if(isForward)
     {
@@ -55,18 +55,14 @@ void moveStraight(double linearSpeed, double distance, bool isForward)
         vel_msg.linear.x = -abs(linearSpeed);
     }
 
-    double start_time = ros::Time::now().toSec();
+    double start_time = ros::WallTime::now().toSec();
     double current_distance = 0.0;
 
-    ros::Rate loop_rate(100);
-
-    while(current_distance<distance)
+    while(current_distance<distance && ros::ok())
     {
         velocity_publisher.publish(vel_msg);
-        double end_time = ros::Time::now().toSec();
+        double end_time = ros::WallTime::now().toSec();
         current_distance = linearSpeed*(end_time - start_time);
-        ros::spinOnce();
-        loop_rate.sleep();
     }
     vel_msg.linear.x = 0;
     velocity_publisher.publish(vel_msg);
@@ -96,17 +92,19 @@ void rotate(double angularSpeedInDegrees, double relativeAngleInDegrees, bool is
     }
 
     double current_angle_in_degrees = 0.0;
-    double start_time = ros::Time::now().toSec();
-    ros::Rate loop_rate(10);
+    double start_time = ros::WallTime::now().toSec();
+    ros::WallRate loop_rate(100);
+	
+	do{        
+		velocity_publisher.publish(vel_msg);
+        double end_time = ros::WallTime::now().toSec();
+		current_angle_in_degrees = angularSpeedInDegrees*(end_time - start_time);
+		ros::spinOnce();
+		loop_rate.sleep();
 
-    while(current_angle_in_degrees<relativeAngleInDegrees)
-    {
-        velocity_publisher.publish(vel_msg);
-        double end_time = ros::Time::now().toSec();
-        current_angle_in_degrees = angularSpeedInDegrees*(end_time - start_time);
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
+	}
+    while(current_angle_in_degrees<relativeAngleInDegrees && ros::ok());
+
     vel_msg.angular.z = 0;
     velocity_publisher.publish(vel_msg);
 }
@@ -115,17 +113,18 @@ void setDesiredOrientation (double desired_angle_degrees)
 {
 	double relative_angle_radians = degrees2radians(desired_angle_degrees) - turtlesim_pose.theta;
 	bool clockwise = ((relative_angle_radians<0)?true:false);
-	rotate(3, radians2degrees(abs(relative_angle_radians)), clockwise);
+	rotate(10, radians2degrees(abs(relative_angle_radians)), clockwise);
 }
 
 void move2Location(turtlesim::Pose  locationPose, double distTolerance)
 {
     geometry_msgs::Twist vel_msg;
 
-	ros::Rate loop_rate(150);
 	double E = 0.0;
 
-    while(getDistance(turtlesim_pose.x, turtlesim_pose.y, locationPose.x, locationPose.y)>distTolerance)
+	ros::WallRate loop(10);
+
+    while(getDistance(turtlesim_pose.x, turtlesim_pose.y, locationPose.x, locationPose.y)>distTolerance && ros::ok())
 	{
 		/****** Proportional Controller ******/
 		//linear velocity in the x-axis
@@ -144,85 +143,152 @@ void move2Location(turtlesim::Pose  locationPose, double distTolerance)
 		velocity_publisher.publish(vel_msg);
 
 		ros::spinOnce();
-		loop_rate.sleep();
-
+		loop.sleep();
 	}
 	vel_msg.linear.x =0;
 	vel_msg.angular.z = 0;
 	velocity_publisher.publish(vel_msg);
 
-	setDesiredOrientation(radians2degrees(locationPose.theta));
 }
 
 void gridMotion()
 {
-    ros::Rate loop(0.5);
+    ros::WallRate loop(1);
 	turtlesim::Pose pose;
 	pose.x=1;
 	pose.y=1;
 	pose.theta=0;
+
 	move2Location(pose, 0.01);
 	loop.sleep();
-	setDesiredOrientation(0);
+	setDesiredOrientation(0.0);
 	loop.sleep();
 
+	// move 9 straight and turn left
 	moveStraight(2.0, 9.0, true);
 	loop.sleep();
 	rotate(10, 90, false);
 	loop.sleep();
-	moveStraight(2.0, 9.0, true);
 
-	rotate(10, 90, false);
-	loop.sleep();
+	// move 1 straight and turn left
 	moveStraight(2.0, 1.0, true);
+	loop.sleep();
 	rotate(10, 90, false);
 	loop.sleep();
-	moveStraight(2.0, 9.0, true);
 
-	rotate(30, 90, true);
+	// move 9 straight and turn right
+	moveStraight(2.0, 9.0, true);
 	loop.sleep();
+	rotate(10, 90, true);
+	loop.sleep();
+
+	// move 1 straight and turn right
 	moveStraight(2.0, 1.0, true);
-	rotate(30, 90, true);
 	loop.sleep();
+	rotate(10, 90, true);
+	loop.sleep();
+
+	// move 9 straight and turn left
 	moveStraight(2.0, 9.0, true);
+	loop.sleep();
+	rotate(10, 90, false);
+	loop.sleep();
 
+	// move 1 straight and turn left
+	moveStraight(2.0, 1.0, true);
+	loop.sleep();
+	rotate(10, 90, false);
+	loop.sleep();
 
-	double distance = getDistance(turtlesim_pose.x, turtlesim_pose.y, x_max, y_max);
+	// move 9 straight and turn right
+	moveStraight(2.0, 9.0, true);
+	loop.sleep();
+	rotate(10, 90, true);
+	loop.sleep();
+
+	// move 1 straight and turn right
+	moveStraight(2.0, 1.0, true);
+	loop.sleep();
+	rotate(10, 90, true);
+	loop.sleep();
+
+	// move 9 straight and turn left
+	moveStraight(2.0, 9.0, true);
+	loop.sleep();
+	rotate(10, 90, false);
+	loop.sleep();
+
+	// move 1 straight and turn left
+	moveStraight(2.0, 1.0, true);
+	loop.sleep();
+	rotate(10, 90, false);
+	loop.sleep();
+
+	// move 9 straight and turn right
+	moveStraight(2.0, 9.0, true);
+	loop.sleep();
+	rotate(10, 90, true);
+	loop.sleep();
+
+	// move 1 straight and turn right
+	moveStraight(2.0, 1.0, true);
+	loop.sleep();
+	rotate(10, 90, true);
+	loop.sleep();
+
+	// move 9 straight and turn left
+	moveStraight(2.0, 9.0, true);
+	loop.sleep();
+	rotate(10, 90, false);
+	loop.sleep();
+
+	// move 1 straight and turn left
+	moveStraight(2.0, 1.0, true);
+	loop.sleep();
+	rotate(10, 90, false);
+	loop.sleep();
+
+	// move 9 straight and turn right
+	moveStraight(2.0, 9.0, true);
+	loop.sleep();
+	rotate(10, 90, true);
+	loop.sleep();
+
+	// move 1 straight and turn right
+	moveStraight(2.0, 1.0, true);
+	loop.sleep();
+	rotate(10, 90, true);
+	loop.sleep();
+
 }
 
 void spiralMotion()
 {
+
     geometry_msgs::Twist vel_msg;
-	double count =0;
-
 	double constant_speed=4;
-	double vk = 1;
-	double wk = 2;
 	double rk = 0.5;
-	ros::Rate loop(1);
 
-	do{
-		rk=rk+1.0;
+	ros::WallRate loop(1);
+
+	while((turtlesim_pose.x<10.5) && (turtlesim_pose.y<10.5) && ros::ok())
+	{
+		rk += 1.0;
 		vel_msg.linear.x =rk;
 		vel_msg.linear.y =0;
 		vel_msg.linear.z =0;
 		//set a random angular velocity in the y-axis
 		vel_msg.angular.x = 0;
 		vel_msg.angular.y = 0;
-		vel_msg.angular.z =constant_speed;//((vk)/(0.5+rk));
+		vel_msg.angular.z =constant_speed;
 
-		std::cout<<"vel_msg.linear.x = "<<vel_msg.linear.x<<std::endl;
-		std::cout<<"vel_msg.angular.z = "<<vel_msg.angular.z<<std::endl;
 		velocity_publisher.publish(vel_msg);
-		ros::spinOnce();
-
 		loop.sleep();
-		//vk = vel_msg.linear.x;
-		//wk = vel_msg.angular.z;
-		//rk = vk/wk;
-		std::cout<<rk<<", "<<vk <<", "<<wk<<std::endl;
-	}while((turtlesim_pose.x<10.5)&&(turtlesim_pose.y<10.5));
-	vel_msg.linear.x =0;
+		ros::spinOnce();
+	}
+
+	vel_msg.linear.x = 0;
+	vel_msg.angular.z = 0;
 	velocity_publisher.publish(vel_msg);
 }
 
