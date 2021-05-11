03df7b935ab54befb6a6f234fb6c158f3abb9a6b
mayankJoshi <31381335+mjoshi07@users.noreply.github.com>
2021-05-11 15:08:30 +0530

    Update gridMotion.cpp

--- src/turtleSim/gridMotion.cpp
+++ src/turtleSim/gridMotion.cpp
@@ -8,10 +8,7 @@ int main(int argc, char** argv)
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
 
-	while(n.ok())
-	{
-		gridMotion();
-	}
+	gridMotion();
 	
 	return 0;
 
