21055b17050ffa2a2e3beb65fb206609bd05b6e0
mayo007 <mayank@i2vsys.com>
2021-05-11 10:17:05 +0530

    Changed folder structure

--- src/turtleSim/gridMotion.cpp
+++ src/turtleSim/gridMotion.cpp
@@ -8,8 +8,11 @@ int main(int argc, char** argv)
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
 
-	gridMotion();
-
+	while(n.ok())
+	{
+		gridMotion();
+	}
+	
 	return 0;
 
 }
--- src/turtleSim/move2Location.cpp
+++ src/turtleSim/move2Location.cpp
@@ -6,18 +6,17 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "turtleBot_move2Location");
 	ros::NodeHandle n;
 	turtlesim::Pose pose;
-	pose.x = 8;
-	pose.y = 4;
-	pose.theta = 0;
+	
+	std::cout<<"Enter x coordinate: \t";
+	std::cin>>pose.x;
+	std::cout<<"Enter y coordinate: \t";
+	std::cin>>pose.y;
+	std::cout<<"Enter theta: \t";
+	std::cin>>pose.theta;
 
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
 
-	move2Location(pose, 0.01);
-
-	pose.x = 5.554;
-	pose.y = 5.554;
-	pose.theta = 0;
 	move2Location(pose, 0.01);
 
 	return 0;
