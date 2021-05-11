6f3d77ef943fa7247dbe308019543fe617907964
mayo007 <mayank@i2vsys.com>
2021-05-11 11:35:56 +0530

    user can enter values to rotate turtlebot at run time

--- src/turtleSim/rotate.cpp
+++ src/turtleSim/rotate.cpp
@@ -4,14 +4,24 @@ int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "turtleBot_rotate");
 	ros::NodeHandle n;
-	double angularSpeedInDegrees = 10;
-	double relativeAngleInDegress = 90;
+	double angularSpeedInDegrees = 10.0;
+	double relativeAngleInDegrees = 90.0;
 	bool isClockwise = true;
+
+	std::cout<<"Enter angular speed (type-double): \t";
+	std::cin>>angularSpeedInDegrees;
+	std::cout<<"\nEnter relative angle (type-double): \t";
+	std::cin>>relativeAngleInDegrees;
+	std::cout<<"\nEnter 1 to rotate clockwise or 0 to rotate counter-clockwise: \t";
+	int direction;
+	std::cin>>direction;
+	isClockwise = direction?true:false;
+	std::cout<<"Rotating...\n";
 
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
 
-	rotate(angularSpeedInDegrees, relativeAngleInDegress, isClockwise);
-
+	rotate(angularSpeedInDegrees, relativeAngleInDegrees, isClockwise);
+	
 	return 0;
 }
