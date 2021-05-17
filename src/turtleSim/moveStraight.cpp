6f2a46069ae5038e6b5ee9b5fce8b6fdafeaeccf
mayankJoshi <31381335+mjoshi07@users.noreply.github.com>
2021-05-17 15:07:59 +0530

    Update moveStraight.cpp

--- src/turtleSim/moveStraight.cpp
+++ src/turtleSim/moveStraight.cpp
@@ -9,35 +9,15 @@ int main(int argc, char **argv)
 	bool isForward = false;
 
 
-	std::cout<<"Enter speed (type-double): \t";
+	std::cout<<"Enter speed: \t";
 	std::cin>>speed;
-	std::cout<<"Enter distance (type-double): \t";
+	std::cout<<"Enter distance: \t";
 	std::cin>>distance;
-
-	bool noValueReceived = false;
-	int i = 5;
-	while(i>0)
-	{
-		i--;
-		std::cout<<" Enter 1 to move forward, 0 to move backwards \t";
-		int direction;
-		std::cin>>direction;
-		if(direction ==0 || direction ==1)
-		{
-			isForward = (direction)?true:false;
-			break;
-		}
-		else
-		{
-			std::cout<<"Enter 1 or 0 only, Tries left "<<i<<'\n';
-			noValueReceived = true;
-		}
-	}
-	if(noValueReceived)
-	{
-		std::cout<<"NO value received for direction, therefore moving forward\n";
-		isForward = true;
-	}
+	std::cout<<"\nEnter 1 to move forward, 0 to move backwards: \t";
+	int direction;
+	std::cin>>direction;
+	isForward = direction?true:false;
+	std::cout<<"Moving...\n";
 
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
