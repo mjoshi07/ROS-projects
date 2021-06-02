a818f6f0f8fd41dd5911bfaa1fe9ebbc764d98ad
mayankJoshi <31381335+mjoshi07@users.noreply.github.com>
2021-06-02 10:08:09 +0530

    comments added

--- src/turtleSim/spiralMotion.cpp
+++ src/turtleSim/spiralMotion.cpp
@@ -2,11 +2,20 @@
 
 int main(int argc, char** argv)
 {
+	// initiate a new ROS node named "turtleBot_spiralMotion"
 	ros::init(argc, argv, "turtleBot_spiralMotion");
+	
+	// create a new node handle
 	ros::NodeHandle n;
 
+	// create a velocity publisher and publish to the topic "/turtle1/cmd_vel"
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+	
+	// create a pose subscriber and subscribe to the topic "/turtle1/pose"
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
 
+	// call the function spiralMotion, you change the default values inside this function
 	spiralMotion();
+	
+	return 0;
 }
