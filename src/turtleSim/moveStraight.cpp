0ea6094bd2bacc7c134bc3704976cbf23dd10bba
mayankJoshi <31381335+mjoshi07@users.noreply.github.com>
2021-06-02 09:55:18 +0530

    Comments added

--- src/turtleSim/moveStraight.cpp
+++ src/turtleSim/moveStraight.cpp
@@ -2,13 +2,18 @@
 
 int main(int argc, char **argv)
 {
+	// initiate a new ROS node named "turtleBot_moveStraight"
 	ros::init(argc, argv, "turtleBot_moveStraight");
+	
+	// create a new node handle
 	ros::NodeHandle n;
-	double speed = 2;
-	double distance = 10;;
-	bool isForward = false;
+	
+	// create variables to store the values
+	double speed;
+	double distance;
+	bool isForward;
 
-
+	// take speed, distance, isForward values as input from user
 	std::cout<<"Enter speed: \t";
 	std::cin>>speed;
 	std::cout<<"Enter distance: \t";
@@ -19,9 +24,13 @@ int main(int argc, char **argv)
 	isForward = direction?true:false;
 	std::cout<<"Moving...\n";
 
+	// create a velocity publisher and publish at the topic "/turtle1/cmd_vel"
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+	
+	// create a pose subscriber and subsribe to the topic "/turtle1/pose"
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
 
+	// call the function moveStraight with the arguments speed, distance, isForward
 	moveStraight(speed, distance, isForward);
 
 	return 0;
