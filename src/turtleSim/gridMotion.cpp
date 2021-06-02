4c35463ef7ca80bad1cd012bc1a791092f72a1f4
mayankJoshi <31381335+mjoshi07@users.noreply.github.com>
2021-06-02 10:05:44 +0530

    comments added

--- src/turtleSim/gridMotion.cpp
+++ src/turtleSim/gridMotion.cpp
@@ -2,12 +2,19 @@
 
 int main(int argc, char** argv)
 {
+	// initiate a new ROS node named "turtleBot_gridMotion"
 	ros::init(argc, argv, "turtleBot_gridMotion");
+	
+	// create a new node handle
 	ros::NodeHandle n;
 
+	// create a velocity publisher and publish to the topic "/turtle1/cmd_vel"
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+	
+	// create a pose subscriber and subscribe to the topic "/turtle1/pose"
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
 
+	// call the function gridMotion, you may change the default values inside this function
 	gridMotion();
 	
 	return 0;
