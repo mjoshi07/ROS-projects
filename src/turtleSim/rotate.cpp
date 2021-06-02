1e93b87bf24c35ddefe2c11e0c94db0da82e0443
mayankJoshi <31381335+mjoshi07@users.noreply.github.com>
2021-06-02 09:59:09 +0530

    comments added

--- src/turtleSim/rotate.cpp
+++ src/turtleSim/rotate.cpp
@@ -2,12 +2,18 @@
 
 int main(int argc, char** argv)
 {
+	// initiate a new ROS node named "turtleBot_rotate"
 	ros::init(argc, argv, "turtleBot_rotate");
+	
+	// create a new node handle
 	ros::NodeHandle n;
-	double angularSpeedInDegrees = 10.0;
-	double relativeAngleInDegrees = 90.0;
-	bool isClockwise = true;
+	
+	// create variables to store the values
+	double angularSpeedInDegrees;
+	double relativeAngleInDegrees;
+	bool isClockwise;
 
+	// take angularSpeedInDegrees, relativeAngleInDegrees, isClockwise values as input from user
 	std::cout<<"Enter angular speed: \t";
 	std::cin>>angularSpeedInDegrees;
 	std::cout<<"\nEnter relative angle (degrees): \t";
@@ -18,9 +24,13 @@ int main(int argc, char** argv)
 	isClockwise = direction?true:false;
 	std::cout<<"Rotating...\n";
 
+	// create a velocity publisher and publish at the topic "/turtle1/cmd_vel"
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+	
+	// create a pose subscriber and subscribe to the topic "/turtle1/pose"
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
 
+	// call the function rotate with the arguments angularSpeedInDegrees, relativeAngleInDegrees, isClockwise
 	rotate(angularSpeedInDegrees, relativeAngleInDegrees, isClockwise);
 	
 	return 0;
