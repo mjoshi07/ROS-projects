0fd4aff06167cedc61a837e7b90191cac0e05337
mayankJoshi <31381335+mjoshi07@users.noreply.github.com>
2021-06-03 17:18:27 +0530

    comments added

--- src/perception/cv/readVideo.cpp
+++ src/perception/cv/readVideo.cpp
@@ -2,20 +2,30 @@
 
 int main()
 {
+    // path to the video file
     std::string vid_src = "./test_video.mp4";
+    
+    // create an Opencv Video Capture Object
     cv::VideoCapture cap(vid_src);
+    
+    // check if video capture object is open
     if (!cap.isOpened())
     {
         return -1;
     }
 
+    // create an Opencv Mat object
     cv::Mat frame;
 
     while (1)
     {
+        // input the video data into opencv mat object, frame
         if(cap.read(frame))
         {
+            // display the frame
             cv::imshow("img", frame);
+            
+            // wait for 1 millisecond and then get a new image
             cv::waitKey(1);
         }
         else
